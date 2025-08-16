1268beda45bb940ef5eaec49cd2f63ba4a143021
Rishu kumar singh <rishu272018@gmail.com>
2025-08-16 20:53:02 +0530

    work on frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react()],
+  css: {
+    postcss: './postcss.config.js', // External config file
+  },
   server: {
     port: 3000,
     proxy: {
       '/api': {
         target: 'http://localhost:5000',
         changeOrigin: true,
-        secure: false
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, ''),
       }
     }
   }
-})
+});
